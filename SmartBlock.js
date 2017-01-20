ef26c5d62641a137e9070c476512b847046ce517
3ds-emu <jonathanlu888@gmail.com>
2017-01-20 13:09:12 +1100

    Added new ad server

--- SmartBlock.js
+++ SmartBlock.js
@@ -3,8 +3,10 @@ chrome.webRequest.onBeforeRequest.addListener(
         return {cancel: true};
     },
     {
-        urls: ["*://*.doubleclick.net/*", "*://www.googleadservices.com/*", "*://*.taboola.com/*"]
+        urls: ["*://*.doubleclick.net/*", "*://www.googleadservices.com/*", "*://*.taboola.com/*", "*://*.adzerk.net/*"]
     },
     ["blocking"]
 );
 
+//This script was specially created for the Chrome browser. It will not work if loaded into any
+//other browser (eg. Firefox).
